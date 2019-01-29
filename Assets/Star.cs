16b7c4a4e7804281275a97753b21f9ff2d553cba
icepol <pavel.koci@icepol.net>
2019-01-29 10:46:00 +0100

    Add stars background

---
+++ Assets/Star.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+    float startPosition;
+    float endPosition;
+    float speed;
+
+    Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+
+        startPosition = Random.Range(3, 6);
+        endPosition = Random.Range(-3, -6);
+        speed = Random.Range(0.5f, 1.5f);
+
+        Vector2 position = transform.position;
+        position.y = startPosition;
+        transform.position = position;
+
+        EventManager.AddListener("StartGame", OnStartGame);
+    }
+
+    void OnDestroy() {
+        EventManager.RemoveListener("StartGame", OnStartGame);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 position = transform.position;
+        position.y -= speed * Time.deltaTime;
+        transform.position = position;
+
+        if (position.y < -6)
+            Destroy(gameObject);
+        else if (position.y < endPosition)
+            animator.SetTrigger("Hide");
+        else if (position.y < startPosition)
+            animator.SetTrigger("Show");
+    }
+
+    void OnStartGame() {
+        // add speed of the walls
+        speed += 2f;
+    }
+}
---
+++ Assets/StarsSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject starPrefab;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn() {
+        while (true) {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+
+            GameObject star = Instantiate(
+                starPrefab,
+                new Vector2(Random.Range(-3, 3), -6),
+                Quaternion.Euler(0, 0, Random.Range(0, 360)),
+                transform
+            );
+        }
+    }
+}
