00d713190a999b587691fe0b77abc703c872b459
icepol <pavel.koci@icepol.net>
2019-05-05 19:01:09 +0200

    More easy color effects

--- Assets/Scripts/PlayerJumpEffect.cs
+++ Assets/Scripts/PlayerJumpEffect.cs
@@ -6,6 +6,7 @@ public class PlayerJumpEffect : MonoBehaviour
 {
     [SerializeField] float zoomSpeed = 1f;
     [SerializeField] float opacitySpeed = 0.1f;
+    [SerializeField] float colorSpeed = 1.1f;
 
     SpriteRenderer spriteRenderer;
     bool isActive = true;
@@ -25,9 +26,16 @@ public class PlayerJumpEffect : MonoBehaviour
         // zoom
         transform.localScale = transform.localScale * (1 + zoomSpeed * Time.deltaTime);
 
-        // make transparent
         Color color = spriteRenderer.color;
-        color.a -= opacitySpeed;
+
+        // make transparent
+        color.a -= opacitySpeed * Time.deltaTime;
+
+        // transform to white
+        color.r = Mathf.Min(color.r + ((1f - color.r) * colorSpeed * Time.deltaTime), 1f);
+        color.g = Mathf.Min(color.g + ((1f - color.g) * colorSpeed * Time.deltaTime), 1f);
+        color.b = Mathf.Min(color.b + ((1f - color.b) * colorSpeed * Time.deltaTime), 1f);
+
         spriteRenderer.color = color;
     }
 
